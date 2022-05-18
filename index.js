8c6c56b395c30aa0535296bbaafc7239d6b45b14
Lucy <lucy.d.bullen@gmail.com>
2022-05-18 11:34:15 -0600

    commit tree bc05ab91f29de6eb6c87a75cb241bc4f62fd635a

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching (drivers, driverSearch){
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === driverSearch.toLowerCase()
+    });
+};
+
+function fuzzyMatch (drivers, driverBeginning) {
+    return drivers.filter(function(driver){
+        return driver.substring(0, driverBeginning.length) === driverBeginning
+    });
+};
+
+function matchName (drivers, driverName) {
+    return drivers.filter(function(driver){
+        return driver.name === driverName;
+    });
+};
